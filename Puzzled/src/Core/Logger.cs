11174bded511682b179e17cf8c0fe836ecae6f34
ggjorven <jorbenvanderwal@gmail.com>
2025-09-15 10:02:26 +0200

    Proof of concept MainMenu.

---
+++ Puzzled/src/Core/Logger.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Puzzled
+{
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // Logger
+    ////////////////////////////////////////////////////////////////////////////////////
+    public class Logger
+    {
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        // Severity enum
+        ////////////////////////////////////////////////////////////////////////////////////
+        public enum Severity
+        {
+            Trace = 0,
+            Info,
+            Warning,
+            Error,
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        // Static methods
+        ////////////////////////////////////////////////////////////////////////////////////
+        public static void Trace(string message) { Log(Severity.Trace, message); }
+        public static void Info(string message) { Log(Severity.Info, message); }
+        public static void Warning(string message) { Log(Severity.Warning, message); }
+        public static void Warn(string message) { Warning(message); }
+        public static void Error(string message) { Log(Severity.Error, message); }
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        // Private static methods
+        ////////////////////////////////////////////////////////////////////////////////////
+        private static ConsoleColor SeverityToColour(Severity severity)
+        {
+            switch (severity)
+            {
+            case Severity.Trace:    return ConsoleColor.White;
+            case Severity.Info:     return ConsoleColor.Green;
+            case Severity.Warning:  return ConsoleColor.Yellow;
+            case Severity.Error:    return ConsoleColor.Red;
+
+            default:
+                break;
+            }
+
+            return ConsoleColor.White;
+        }
+
+        private static string SeverityToTag(Severity severity)
+        {
+            switch (severity)
+            {
+            case Severity.Trace:    return "TRACE";
+            case Severity.Info:     return "INFO";
+            case Severity.Warning:  return "WARN";
+            case Severity.Error:    return "ERROR";
+
+            default:
+                break;
+            }
+
+            return "<UNKNOWN>";
+        }
+
+        private static void Log(Severity severity, string message)
+        {
+            Console.ForegroundColor = SeverityToColour(severity);
+            Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] [{SeverityToTag(severity)}]: {message}");
+            Console.ResetColor();
+        }
+
+    }
+
+}
--- Puzzled/src/Game/Scenes/MainMenu.xaml.cs
+++ Puzzled/src/Game/Scenes/MainMenu.xaml.cs
@@ -36,29 +36,65 @@ namespace Puzzled
             m_Loaded = true;
             m_Renderer = new Renderer(GameCanvas);
 
-            m_GameName = new UI.Text(
-                "Puzzled Skeleton",
-                64.0f,
+            // Game name
+            {
+                m_GameName = new UI.Text(
+                    "Puzzled Skeleton",
+                    64.0f,
 
-                "Courier New",
-                new Maths.Vector2(0.0f, 0.0f)
-            );
+                    "Courier New",
+                    new Maths.Vector2(0.0f, 0.0f)
+                );
 
-            m_GameName.Position = UI.Utils.GetCenter(UICanvas, m_GameName.UIElement);
-            m_GameName.AddToCanvas(UICanvas);
+                Maths.Vector2 center = UI.Utils.GetCenter(UICanvas, m_GameName.UIElement);
+                m_GameName.Position = new Maths.Vector2(center.X, Game.Instance.Window.Height);
+                m_GameName.AddToCanvas(UICanvas);
+            }
+
+            // Press start
+            {
+                m_PressStart = new UI.Text(
+                    "Press Start",
+                    32.0f,
+
+                    "Courier New",
+                    new Maths.Vector2(0.0f, 0.0f)
+                );
+
+                Maths.Vector2 center = UI.Utils.GetCenter(UICanvas, m_PressStart.UIElement);
+                m_PressStart.Position = new Maths.Vector2(center.X, center.Y + 200.0f);
+                m_PressStart.AddToCanvas(UICanvas);
+            }
         }
 
         public void OnUpdate(float deltaTime)
         {
-            if (m_Loaded)
+            // UI updates
+            if (m_Loaded && m_GameName.Position.Y > 157.0f) // TODO: Remove magic number
             {
-                m_GameName.Position = new Maths.Vector2(m_GameName.Position.X, m_GameName.Position.Y - (50.0f * deltaTime));
+                m_GameName.Position = new Maths.Vector2(m_GameName.Position.X, m_GameName.Position.Y - (m_UIVelocity * deltaTime));
+            }
+
+            // Note: Ugly ass code // Note 2: Proof of concept
+            m_CurrentFlashTimer += deltaTime;
+            if (m_CurrentFlashTimer >= m_FlashTime)
+            {
+                if (m_PressStart.UIElement.Foreground == Brushes.White)
+                {
+                    m_PressStart.UIElement.Foreground = Brushes.Black;
+                }
+                else
+                {
+                    m_PressStart.UIElement.Foreground = Brushes.White;
+                }
+                m_CurrentFlashTimer = 0.0f;
             }
         }
 
         public void OnRender()
         {
             // TODO: Render some sort of background?
+            // TODO: Flash press start
         }
 
         public void OnUIRender()
@@ -67,6 +103,10 @@ namespace Puzzled
 
         public void OnEvent(Event e)
         {
+            if (e is MouseButtonPressedEvent mbe)
+            {
+                Logger.Trace($"Position {{ .x = {m_GameName.Position.X}, .y = {m_GameName.Position.Y} }}");
+            }
         }
 
         ////////////////////////////////////////////////////////////////////////////////////
@@ -75,8 +115,14 @@ namespace Puzzled
         private bool m_Loaded = false; // TODO: Find a better way
         private Renderer m_Renderer;
 
-        private UI.Text m_GameName;
+        // Animation
+        private const float m_UIVelocity = 100.0f;
         
+        private UI.Text m_GameName; // End: 157
+        private UI.Text m_PressStart;
+
+        private const float m_FlashTime = 0.4f;
+        private float m_CurrentFlashTimer = 0.0f;
 
     }
 
