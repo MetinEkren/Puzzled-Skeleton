c0b7c3db13273872de9b1f7b9bc099e23e4b36e5
ggjorven <jorbenvanderwal@gmail.com>
2025-09-18 10:26:12 +0200

    Remove unused functions.

--- Puzzled/src/UI/Utils.cs
+++ Puzzled/src/UI/Utils.cs
@@ -11,9 +11,9 @@ namespace Puzzled.UI
 {
 
     ////////////////////////////////////////////////////////////////////////////////////
-    // Fonts
+    // Utils
     ////////////////////////////////////////////////////////////////////////////////////
-    public class Utils // Note: A static font cache
+    public class Utils
     {
 
         ////////////////////////////////////////////////////////////////////////////////////
@@ -28,35 +28,6 @@ namespace Puzzled.UI
             double y = (canvas.ActualHeight - uiElement.DesiredSize.Height) / 2;
 
             return new Maths.Vector2((float)x, (float)y);
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////////
-        // Static getters // Note: DIPs are Device Independent Pixels 
-        // Note 2: These functions are ChatGPT generated so don't blame me if it doesn't work (Jorben)
-        // TODO: Check functionality
-        ////////////////////////////////////////////////////////////////////////////////////
-        public static double ConvertDIPsToPixels(double dips, Visual visual)
-        {
-            // Get the DPI info from the visual's presentation source
-            var source = PresentationSource.FromVisual(visual);
-            if (source?.CompositionTarget != null)
-            {
-                var dpiX = 96.0 * source.CompositionTarget.TransformToDevice.M11;
-                return dips * source.CompositionTarget.TransformToDevice.M11;
-            }
-
-            return dips;
-        }
-
-        public static double ConvertPixelsToDIPs(double pixels, Visual visual)
-        {
-            var source = PresentationSource.FromVisual(visual);
-            if (source?.CompositionTarget != null)
-            {
-                return pixels / source.CompositionTarget.TransformToDevice.M11;
-            }
-
-            return pixels;
         }
 
     }
