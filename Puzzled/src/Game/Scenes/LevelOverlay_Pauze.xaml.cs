12925b03666dc2875a11aa1202fc42e0d03e5a26
metin <239001@student.firda.nl>
2025-10-08 12:20:29 +0200

    pauze menu wpf

---
+++ Puzzled/src/Game/Scenes/LevelOverlay_Pauze.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Puzzled
+{
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // LevelOverlay_Pauze
+    ////////////////////////////////////////////////////////////////////////////////////
+    public partial class LevelOverlay_Pauze : UserControl
+    {
+
+        
+    }
+
+}
